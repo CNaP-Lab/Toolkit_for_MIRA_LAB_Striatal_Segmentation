225c83c30fc36d8e961d3463cd452f1be7b8d90a
jocwilliams <31212333+jocwilliams@users.noreply.github.com>
2022-12-15 12:25:58 -0500

    File name and documentation changes

--- README.md
+++ README.md
@@ -18,7 +18,7 @@ This readme.txt covers a pipeline that produces CNN-based segmentations of the s
 
 This readme.txt will detail how this pipeline works (in a step-by-step fashion), running instructions, the dependencies required, and the parameters the user must adjust. This pipeline is based in MATLAB and python; much of the editable components are written in MATLAB. 
 
-The main operating script of the pipeline is CNNStriatalSegmentation.m. This script is called by CNNStriatalSegmentation_call.m, which has a set of parameters that the user is instructed to adjust therein. Full running operations are discussed in the following section. 
+The main operating script of the pipeline is main_CNNStriatalSegmentation.m. This script is called by CNNStriatalSegmentation_wrapper_script.m, which has a set of parameters that the user is instructed to adjust therein. Full running operations are discussed in the following section. 
 
 
 II. RUNNING INSTRUCTIONS:
@@ -28,9 +28,9 @@ II. RUNNING INSTRUCTIONS:
 
 3. Within the checkpoint file, please replace “/mnt/jxvs2_02/neil/StriatalSegmentation/“ to where your subfolder StriatalSegmentation is located.
 
-4. Adjust the necessary parameters in CNNStriatalSegmentation_call.m; supply the paths for the files and subfolders as instructed therein and as described below.
+4. Adjust the necessary parameters in CNNStriatalSegmentation_wrapper_script.m; supply the paths for the files and subfolders as instructed therein and as described below.
 
-	For each run of the pipeline involving different subjects, the following are required and the paths must be adjusted:						segmentation_intermediate_directory,T1_acpc_dc_restore_brain,nat_acpc_brainmask, and BOLD_template_image. 
+	For each run of the pipeline involving different subjects, the following are required and the paths must be adjusted:   segmentation_intermediate_directory,T1_acpc_dc_restore_brain,nat_acpc_brainmask, and BOLD_template_image. 
 		
 		Segmentation_intermediate_directory refers to the directory where all intermediate and final outputs of this CNN pipeline will be saved for each subject run. 
 
@@ -40,29 +40,23 @@ II. RUNNING INSTRUCTIONS:
 
 		Bold_template_image refers to the path of the bold functional MRI image relating to the subject used for this run.
 	
-5. Ensure SPM12 and tippVol are on your path in MATLAB and run the script CNNStriatalSegmentation_call.m. 
+5. Ensure SPM12 and tippVol are on your path in MATLAB and run the script CNNStriatalSegmentation_wrapper_script.m. 
 
-6. You may now inspect your final striatal segmentations for both your structural and functional images, found in the segmentation_intermediate_directory (whose path you edited in CNNStriatalSegmentation_call.m from step 5), in an image viewer of your choice. Our team used MRIcron, a free tool readily available at: https://www.nitrc.org/projects/mricron. The directory also contains intermediates generated in the pipeline, which may be viewed. 
+6. You may now inspect your final striatal segmentations for both your structural and functional images, found in the segmentation_intermediate_directory (whose path you edited in CNNStriatalSegmentation_wrapper_script.m from step 5), in an image viewer of your choice. Our team used MRIcron, a free tool readily available at: https://www.nitrc.org/projects/mricron. The directory also contains intermediates generated in the pipeline, which may be viewed. 
 
 III. PIPELINE STEPS
 
-1. The main script (CNNStriatalSegmentation.m) reads arguments given by the user in CNNStriatalSegmentation_call.m for the paths of two categories of objects: the inputs to the pipeline and internal files/working directories for the pipeline.
+1. The main script (main_CNNStriatalSegmentation.m) reads arguments given by the user in CNNStriatalSegmentation_wrapper_script.m for the paths of two categories of objects: the inputs to the pipeline and internal files/working directories for the pipeline.
 2. The structural MRI image (T1) is rotated 90 degree (with the getRotatedCNN_image subfunction).
 3. The brain mask is rotated 90 degrees (with the getRotatedCNN_image subfunction).
 4. The rotated T1 image is resliced according to the resolution of the CNN reslice template, using 7th degree spline interpolation in SPM (with the getReslicedCNN_image subfunction). In this reslicing run as well as in all future runs, wrapping is turned on in the x, y, and z directions. 
 5. The brain mask is resolved according to the resolution of the CNN reslice template, using nearest neighbor interpolation in SPM (with the getReslicedCNN_image subfunction).
 6. The brain mask is padded. The padding is governed by a 3-D cubic structuring element whose width is set at 3 pixels. The line, structuringElement = strel('cube',3), may be edited if the user seeks a bigger width of the structuring element or desires to use a different geometric shape. These changes may have an effect on the amount of segmentations observed. Excessive padding can erode the segmentations while no padding can introduce background signals that can blend with striatal segmentations. 
-7. Through the function pythonCNNstriatalSegmentation, the main script executes a python script (orig_mod_NNEval.py) that uses previously trained network weights to generate segmentations for the input T1 image. The T1 image is padded and 2 dimensions on each side of the image are added to achieve a 5 dimensional object [1x256x256x192x1]. 
-
-	The output of this python script is a .mat file, which contains 2 variables, out, which contains the raw segmentations, and mri, which contains 	the original image. After the python script returns the outputs, the out variable is squeezed.
-
-8. This .mat file is processed with the segmentation_postprocessing subfunction. Out has a size  of 256x256x192x6, where 6 represents the segmentation layers, including 1 for background. In the first step, the padded elements are removed and out’s size changes to 234x234x156x6. Next, the cnn network produced probability distributions ranging from 0-1 are converted into discrete values (0 or 1). Then, instead of each voxel being assigned to a probability estimate based on the likelihood of being in each striatal layer, each voxel is only assigned to 1 striatal region using the max function. This avoids overlapping segmentations and ensures each striatal region is specific and based on voxels that exist in that region. 
-
+7. Through the function pythonCNNstriatalSegmentation, the main script executes a python script (orig_mod_NNEval.py) that uses previously trained network weights to generate segmentations for the input T1 image. The T1 image is padded and 2 dimensions on each side of the image are added to achieve a 5 dimensional object [1x256x256x192x1]. The output of this python script is a .mat file, which contains 2 variables, out, which contains the raw segmentations, and mri, which contains the original image. After the python script returns the outputs, the out variable is squeezed.
+8. This .mat file is processed with the segmentation_postprocessing subfunction. Out has a size of 256x256x192x6, where 6 represents the segmentation layers, including 1 for background. In the first step, the padded elements are removed and out’s size changes to 234x234x156x6. Next, the cnn network produced probability distributions ranging from 0-1 are converted into discrete values (0 or 1). Then, instead of each voxel being assigned to a probability estimate based on the likelihood of being in each striatal layer, each voxel is only assigned to 1 striatal region using the max function. This avoids overlapping segmentations and ensures each striatal region is specific and based on voxels that exist in that region. 
 9. The image containing the striatal segmentations is rotated 90 degrees in the direction opposite of that from step 2. 
-
 10. The segmentations are resliced according to the resolution of the original T1 weighted MRI image input using 7th degree spline interpolation in SPM.
-
-11. An additional output image is generated so that the segmentations following step 9 are resliced according to the resolution of the BOLD images the user provided, using 7th degree spline interpolation. 
+11. An additional output image is generated so that the segmentations following step 9 are resliced according to the resolution of the BOLD images the user provided, using 7th degree spline interpolation. This is the final nifti output intended for use with BOLD data.
 
 IV. REQUIRED DEPENDENCIES, PYTHON VERSION, AND OTHER FILES
 
@@ -78,6 +72,6 @@ Nibabel: 4.0.2
 
 Scipy: 1.9.
 
-SPM12 must be installed prior to running the pipeline. 
+SPM12 must be installed and on the MATLAB path prior to running the pipeline. 
 
 Additionally, in order to run this pipeline, a T1-weighted structural MRI image in ACPC orientation, a NAT brain mask in ACPC orientation, and a bold functional MRI image are required, as described in step 4 in RUNNING INSTRUCTIONS. 
---
+++ main_CNNStriatalSegmentation.m
@@ -0,0 +1,238 @@
+function [store] = main_CNNStriatalSegmentation(varargin)
+
+    m_file_name_and_path=mfilename('fullpath');
+    [m_file_directory,~,~]=fileparts(m_file_name_and_path);
+    segmentation_python_code_filename='orig_mod_NNEval.py';
+    segmentation_python_code=fullfile(m_file_directory,segmentation_python_code_filename);
+    % Segmentation_python_code refers to the the path of the script orig_mod_NNEval.py, which is included in the github repository. The script uses previously %generated trained network weights to predict striatal segmentations for input MRI & fMRI images.
+
+    segmentation_directory_name='StriatalSegmentation';
+    segmentation_directory=fullfile(m_file_directory,segmentation_directory_name);
+    % Segmentation_directory refers to the StriatalSegmentation folder, included in the github repository. The folder includes the below reslice_template.nii file as well as 4 files that are used by the python script for determining CNN network weights.
+
+    CNN_reslice_template_filename='reslice_template.nii';
+    CNN_reslice_template=fullfile(m_file_directory,'StriatalSegmentation',CNN_reslice_template_filename);
+    % CNN_reslice_template refers to the path of reslice_template.nii, which is included in the github repository. This is a nifti image file provided so that
+    %input images can be resliced according to the nii's resolution, which is the resolution expected by the python script, orig_mod_NNEval.py
+
+    % Store, a struct, saves the file names and image types of
+    % all final and intermediate outputs generated during this pipeline run.
+    store.fname{1}='null';
+    store.imagetype{1}='null';
+    %% end of new part
+    numArgIn = length(varargin);
+    currentArgNumber = 1;
+    while (currentArgNumber <= numArgIn)
+        StringCurrentArg = (string(varargin{currentArgNumber}));
+        numToAdd = 2;
+        switch(StringCurrentArg)
+            case "T1_acpc_dc_restore_brain"
+                T1_acpc_dc_restore_brain = varargin{currentArgNumber + 1};
+            case "nat_acpc_brainmask"
+                nat_acpc_brainmask = varargin{currentArgNumber + 1};
+            case "segmentation_intermediate_directory"
+                segmentation_intermediate_directory = varargin{currentArgNumber + 1};
+            case "BOLD_template_image"
+                BOLD_template_image = varargin{currentArgNumber + 1};
+            otherwise
+                error("Unrecognized input argument")
+        end
+        currentArgNumber = currentArgNumber + numToAdd;
+    end
+    disp('Read all arguments')
+
+    imageType = 'striatalCNNrotated_NATt1brain';
+    [store,rotatedCNN_T1] = getRotatedCNN_image(store,T1_acpc_dc_restore_brain,imageType,'toCNN');
+    imageType = 'striatalCNNrotated_NATbrainmask';
+    [store,rotatedCNN_brainmask] = getRotatedCNN_image(store,nat_acpc_brainmask,imageType,'toCNN');
+
+
+    imageType = 'striatalCNNres_striatalCNNrotated_NATt1brain';
+    isMask = false;
+    imagePrefix = 'striatalCNNres_';
+    [store,reslicedRotatedCNN_T1] = getReslicedCNN_image(store,rotatedCNN_T1,CNN_reslice_template,imageType,imagePrefix,isMask);
+    imageType = 'striatalCNNres_striatalCNNrotated_NATbrainmask';
+    isMask = true;
+    imagePrefix = 'striatalCNNres_';
+    [store,reslicedRotatedCNN_brainmask] = getReslicedCNN_image(store,rotatedCNN_brainmask,CNN_reslice_template,imageType,imagePrefix,isMask);
+
+    segmentation_python_output_intermediate_filename = 'CNN_striatal_python_output_intermediate.mat';
+    segmentation_python_output_intermediate_fullpath = fullfile(segmentation_intermediate_directory , segmentation_python_output_intermediate_filename);
+
+    % Padding the brain mask
+    [VV_reslicedRotatedCNN_brainmask,YY_reslicedRotatedCNN_brainmask] = tippVol(reslicedRotatedCNN_brainmask);
+    % Set NaN values to 0
+    YY_reslicedRotatedCNN_brainmask(isnan(YY_reslicedRotatedCNN_brainmask)) = 0;
+
+    % The following is the default setting created by john, at "3"
+    %     structuringElement = strel('cube',3);
+    % Keep the same as above if you want the default setting.
+    structuringElement = strel('cube',3);
+
+    padded_brain_mask = logical(YY_reslicedRotatedCNN_brainmask);
+    eroded_padded_brain_mask = imerode(padded_brain_mask,structuringElement); %3D erosion once
+
+    [out,mri] = pythonCNNstriatalSegmentation(segmentation_python_code , reslicedRotatedCNN_T1, segmentation_python_output_intermediate_fullpath, segmentation_directory);
+
+    [store,raw_segmentation_filename] = segmentation_postprocessing(store,out,mri,eroded_padded_brain_mask,VV_reslicedRotatedCNN_brainmask,segmentation_intermediate_directory);
+
+    imageType = 'unrotated_striatalCNN_segmentation';
+    [store,unrotatedCNN_segmentation] = getRotatedCNN_image(store,raw_segmentation_filename,imageType,'toACPC');
+
+    imageType = 'anatRes_NATspace_striatalCNNparcels';
+    imagePrefix = [imageType '_'];
+    isMask = true;
+    [store,anatRes_NATspace_striatalCNNparcels] = getReslicedCNN_image(store,unrotatedCNN_segmentation,T1_acpc_dc_restore_brain,imageType,imagePrefix,isMask);
+
+    imageType = 'BOLDRes_NATspace_striatalCNNparcels';
+    imagePrefix = [imageType '_'];
+    isMask = true;
+    [store,BOLDRes_NATspace_striatalCNNparcels] = getReslicedCNN_image(store,unrotatedCNN_segmentation,BOLD_template_image,imageType,imagePrefix,isMask);
+
+    disp('Full striatal segmentation pipeline complete.');  pause(eps); drawnow;
+
+
+
+    function [store,rotatedFileText] = getRotatedCNN_image(store,T1_filename,imageType,direction)
+        %Rotate 90 deg
+        [a,b,c] = fileparts(T1_filename);
+        if (strcmpi(direction,'toCNN'))
+            ang = pi/2;
+            rotatedFileText = fullfile(a, ['striatalCNNrotated_' b c]);
+        elseif (strcmpi(direction,'toACPC'))
+            ang = -pi/2;
+            rotatedFileText = fullfile(a, ['striatalCNN_unrotated_' b c]);
+        end
+        MM = [1 0 0 0; 0 cos(ang) sin(ang) 0; 0 -sin(ang) cos(ang) 0; 0 0 0 1];
+        [VV,YY] = tippVol(T1_filename);
+
+        VV.mat = MM * VV.mat;
+
+        VV.fname = rotatedFileText;
+        if( exist(rotatedFileText,'file') )
+            warning(['Overwriting file : ' rotatedFileText]); pause(eps); drawnow;
+            delete(rotatedFileText); pause(eps); drawnow;
+        end
+        spm_write_vol(VV,YY); pause(eps); drawnow;
+
+
+        [a,b,c] = fileparts(VV.fname);
+        imagefname = [b c];
+        store.fname{end+1}=imagefname;
+        store.imagetype{end+1}=imageType;
+
+
+    end
+
+    function [store,reslicedRotatedCNN_T1] = getReslicedCNN_image(store,source_T1,reslice_template,imageType,imagePrefix,isMask)
+        %Reslice to the resolution desired by the CNN python script
+        %If is a mask, will use nearest neighbor interpolation.  Make sure
+        %isMask = true!
+        %If not, uses 7th degree spline interpolation.
+        if(isMask)
+            slicejob{1}.spm.spatial.coreg.write.roptions.interp = 0; %Nearest neighbor interpolation
+        else
+            slicejob{1}.spm.spatial.coreg.write.roptions.interp = 7; %7th degree spline interpolation
+        end
+        slicejob{1}.spm.spatial.coreg.write.ref = {[reslice_template ',1']};
+
+        slicejob{1}.spm.spatial.coreg.write.source = {[source_T1 ',1']};
+        slicejob{1}.spm.spatial.coreg.write.roptions.wrap = [1,1,1];
+        slicejob{1}.spm.spatial.coreg.write.roptions.mask = 0;
+        slicejob{1}.spm.spatial.coreg.write.roptions.prefix = imagePrefix;
+        [sourcePath,sourceName,sourceExtension] = fileparts(source_T1);
+        reslicedRotatedCNN_T1 = fullfile(sourcePath, [imagePrefix sourceName sourceExtension]);
+        if( exist(reslicedRotatedCNN_T1,'file') )
+            warning(['Overwriting file : ' reslicedRotatedCNN_T1]); pause(eps); drawnow;
+            delete(reslicedRotatedCNN_T1); pause(eps); drawnow;
+        end
+        spm_jobman('run',{slicejob(1)}); pause(eps); drawnow;
+
+        [a,b,c] = fileparts(reslicedRotatedCNN_T1);
+        imagefname = [b c];
+        store.fname{end+1}=imagefname;
+        store.imagetype{end+1}=imageType;
+
+
+    end
+
+
+    function [out,mri] = pythonCNNstriatalSegmentation(segmentation_python_code , T1_acpc_restore_brain, segmentation_python_output_intermediate_fullpath, segmentation_network_weights_directory)
+        disp('Deploying CNN Striatal Segmentation python script.'); pause(eps); drawnow;
+
+        %call the python script that generates the striatal segmentations,
+        %requring arg1, arg2, arg3.
+        if(exist(segmentation_python_output_intermediate_fullpath,'file'))
+            delete(segmentation_python_output_intermediate_fullpath); pause(eps); drawnow;
+        end
+        pythonCallString = ['python3 ' segmentation_python_code ' ' T1_acpc_restore_brain ' ' segmentation_python_output_intermediate_fullpath ' ' segmentation_network_weights_directory];
+        [status,cmdout] = system(pythonCallString,'-echo'); pause(eps); drawnow;
+        a = load(segmentation_python_output_intermediate_fullpath);
+        out = squeeze(a.out);
+        mri = a.mri;
+
+        pause(eps); drawnow;
+    end
+
+    function [store,raw_segmentation_filename] = segmentation_postprocessing(store,out,mri,eroded_padded_brain_mask,VV_reslicedRotatedCNN_brainmask,segmentation_intermediate_directory)
+        % After previous step, we get a.out in size of 256x256x192x6, where
+        % 6 represents the segmentation layers, including 1 for background.
+
+        %     disp('Beginning striatal CNN segmentation post-processing.');
+        x = size(out,1) - size(mri,1);
+        out(1:x,:,:,:) = [];
+        y = size(out,2) - size(mri,2);
+        out(:,1:y,:,:) = [];
+        z = size(out,3) - size(mri,3);
+        out(:,:,1:z,:) = [];
+        %     After last 3 lines, a.out's size changes to {{234x234x156x6}} since
+        %     since we eliminated the padded elements. {{value}} depends on the original
+        %     input image size/dimensions.
+
+        %The cnn network produces probability distributions (whether the
+        %voxels are striatal or not) so these values range from 0-1 and are
+        %continuous decimal values, so in the next steps, we will change the
+        %probability distributions into discrete values (0 or 1).
+        out = out .*repmat(mri>0, [1,1,1,1,size(out,5)]);
+
+        tf_out_modified = zeros(size(mri));
+
+        % The below is meant to take care of having the sixth
+        % segmentation. We end up with 5 striatal regions and neglect
+        % background. That's what id-1 is meant for. Before that line,
+        % we also give each vox an id # representing the striatal region
+        % instead of the previous probability estimate. Also, each voxel is
+        % only assigned to 1 stratial region based on the max function.
+        for vox = 1:numel(tf_out_modified)
+            %         vox
+            [xx,yy,zz] = ind2sub(size(tf_out_modified),vox);
+            [v,id] = max(out(xx,yy,zz,:));
+            tf_out_modified(vox) = id - 1;
+        end
+        out = tf_out_modified;
+
+        %   Remove anything outside of the brain mask, eroded by one voxel.
+        outsideOfMask = ~eroded_padded_brain_mask;
+        out(outsideOfMask) = 0;
+        % disp('erosion removed here');
+
+        raw_segmentation_filename = [segmentation_intermediate_directory '/' 'raw_StriatalCNNparcels.nii'];
+
+        VV_reslicedRotatedCNN_brainmask.fname = raw_segmentation_filename;
+        if( exist(raw_segmentation_filename,'file') )
+            warning(['Overwriting file : ' raw_segmentation_filename]); pause(eps); drawnow;
+            delete(raw_segmentation_filename); pause(eps); drawnow;
+        end
+        spm_write_vol(VV_reslicedRotatedCNN_brainmask,out); pause(eps); drawnow;
+
+        imageType = 'raw_StriatalCNNparcels';
+        %
+        [a,b,c] = fileparts(VV_reslicedRotatedCNN_brainmask.fname);
+        imagefname = [b c];
+
+        store.fname{end+1}=imagefname;
+        store.imagetype{end+1}=imageType;
+
+
+    end
+end
