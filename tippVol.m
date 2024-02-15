87159ef9aa87141846f108d6cc46eb35024d0b2c
yashpat016 <85250036+yashpat016@users.noreply.github.com>
2024-02-15 16:37:01 -0500

    Updated tippVol to match TL_Development version dated 12192023

--- tippVol.m
+++ tippVol.m
@@ -16,15 +16,18 @@ if nargout > 1
         V = {V};
     end
     
+    Y = cell(length(V),1);
     for i = 1:length(V)
         Y{i} = spm_read_vols(V{i});
         for j = 1:length(V{i})
             V{i}(j).dt = [16 0]; %cast structure to FLOAT32 so we don't end up with crappy image when we write new data with V structure
         end
         Y{i} = double(Y{i}); %cast to double so that we don't get stupid rounding errors when mathing about
-        if V{i}(1).mat(1) > 0
-            V{i}(1).mat(1,:) = -V{i}(1).mat(1,:);
-            Y{i} = Y{i}(end:-1:1,:,:,:);
+        for j = 1:length(V{i})
+            if ( V{i}(j).mat(1) > 0 )
+                V{i}(j).mat(1,:) = -V{i}(j).mat(1,:);
+                Y{i}(:,:,:,j) = Y{i}(end:-1:1,:,:,j);
+            end
         end
     end
     
